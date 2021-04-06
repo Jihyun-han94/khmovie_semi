4c7dd9df248986ec7e4b3908aeb02bf89aef20d3
you97925 <you97925@host.docker.internal>
2021-04-06 15:32:42 +0900

    04.06

---
+++ myweb/src/Member/MemberVO.java
@@ -0,0 +1,15 @@
+package Member;
+
+import java.sql.Date;
+
+public class MemberVO {
+	private String user_id = null;
+	private String user_pw = null;
+	private String user_name = null;
+	private String gender = null;
+	private String email = null;
+	private String phone_number =null;
+	private int purchase;
+	private int grande;
+	private Date birth_date = null;
+}
