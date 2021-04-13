c3f355b67c180d844e2c6a9d4695a5598eb95761
한지현 <gkswlgus923@gamil.com>
2021-04-13 11:00:34 +0900

    디비수정

--- myweb/sql/PROJECT_TABLE.sql
+++ myweb/sql/PROJECT_TABLE.sql
@@ -9,11 +9,29 @@ CREATE TABLE Movie (
     ARTIST  VARCHAR2(100)
 );
 
-CREATE TABLE Reserve (
-    R_ID VARCHAR2(10) NOT NULL,
-    R_TITLE VARCHAR2(100),
-    R_PRICE NUMBER NOT NULL,
-    CONSTRAINTS PK_RESERVE PRIMARY KEY (R_ID, R_TITLE)
+CREATE TABLE theater_schedule (
+    scheduleID number CONSTRAINT PK_Theater_Sche_ScheID PRIMARY KEY,
+    theaterName varchar2(50) NOT NULL,
+    time_schedule varchar2(20) NOT NULL,
+    seatNum varchar2(20) NOT NULL
+);
+
+CREATE TABLE ticket (
+    ticketID number CONSTRAINT PK_Ticket_TicketID_PK PRIMARY KEY,
+    holdDate varchar2(20) NOT NULL,
+    scheduleID number NOT NULL,
+    title varchar2(100) NOT NULL,
+    booked char(5) CHECK (booked IN ('Y')),
+    CONSTRAINT FK_Ticket_ScheID FOREIGN KEY (scheduleID) REFERENCES theater_schedule (scheduleID) ON DELETE CASCADE,
+    CONSTRAINT FK_Ticket_Title FOREIGN KEY (title) REFERENCES movie (title) ON DELETE CASCADE
+);
+
+CREATE TABLE ticket_status (
+    ticketID number CONSTRAINT PK_Status_TID PRIMARY KEY,
+    user_id varchar2(10) NOT NULL,
+    Btime date default sysdate NOT NULL,
+    CONSTRAINT FK_Status_TID FOREIGN KEY (ticketID) REFERENCES ticket (ticketID),
+    CONSTRAINT FK_Status_UID FOREIGN KEY (user_id) REFERENCES user_info (user_id)
 );
 
 CREATE TABLE User_info (
@@ -28,6 +46,7 @@ CREATE TABLE User_info (
     BIRTH_DATE DATE
 );
 
+CREATE SEQUENCE board_seq;
 CREATE TABLE Board_t (
     B_NUM NUMBER PRIMARY KEY,
     B_ID VARCHAR2(20) NOT NULL,
@@ -40,8 +59,20 @@ CREATE TABLE Board_t (
     CONSTRAINTS FK_TITLE FOREIGN KEY (B_TITLE) REFERENCES Movie (TITLE)
 );
 
-CREATE SEQUENCE board_seq;
+CREATE TABLE BOARD_COMMENT 
+(
+  CNUM NUMBER PRIMARY KEY,
+  CID VARCHAR2(15),
+  CDATE DATE,
+  CPARENTNUM NUMBER,
+  CCONTENT VARCHAR2(500) NOT NULL,
+  CCOUNT NUMBER
+);
+create sequence COMMENT_SEQ;
 
-ALTER TABLE User_info MODIFY(GENDER NUMBER);
-ALTER TABLE User_info MODIFY(USER_PW VARCHAR2(40));
-ALTER TABLE User_info MODIFY(BIRTH_DATE VARCHAR2(20));
+CREATE TABLE FILE_T(
+    movietitle VARCHAR(200),
+    fileName VARCHAR(200),
+    fileRealName VARCHAR(200)
+);
+
